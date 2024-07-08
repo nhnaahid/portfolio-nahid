7f30d814da3c5222f0d591c38ee8773c43bd9a3a
nhnaahid <nhnahid.iu@gmail.com>
2024-07-08 23:02:47 +0600

    add: project details button

--- src/pages/Home/Projects/Projects.jsx
+++ src/pages/Home/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import skillup from '../../../assets/projects/skillup.png'
 import alter from '../../../assets/projects/alterwhite.png'
 import krafti from '../../../assets/projects/krafti.png'
+import { GoArrowUpRight } from 'react-icons/go';
 
 const Projects = () => {
     return (
@@ -12,8 +13,13 @@ const Projects = () => {
                     <div className="w-full h-full">
                         <img className='w-full h-full object-cover' src={skillup} alt="" />
                     </div>
-                    <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t from-sky-700 to-sky-100 opacity-80 text-white p-4 transform translate-y-full group-hover:translate-y-0 transition-transform duration-500 h-full">
-                        <p className='text-blue-900 font-bold font-merri'>SkillUp</p>
+                    <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t from-sky-700 to-sky-100 opacity-90 text-white p-4 transform translate-y-full group-hover:translate-y-0 transition-transform duration-500 h-full">
+                        <div className='flex flex-col items-center justify-center w-full h-full space-y-1'>
+                            <h2 className='text-black font-bold font-merri text-center text-2xl'>SkillUp</h2>
+                            <p className='text-black font-bold font-merri text-center pb-2'>Online Educational Course Management System</p>
+                            <GoArrowUpRight className='text-center text-4xl bg-sky-700 rounded-full p-1 cursor-pointer hover:bg-sky-300 hover:text-black duration-300'/>
+                        </div>
+
                     </div>
                 </div>
                 {/* alterwhite */}
@@ -21,7 +27,7 @@ const Projects = () => {
                     <div className="w-full h-full">
                         <img className='w-full h-full object-cover' src={alter} alt="" />
                     </div>
-                    <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t from-sky-700 to-sky-100 opacity-80 text-white p-4 transform translate-y-full group-hover:translate-y-0 transition-transform duration-500 h-full">
+                    <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t from-sky-700 to-sky-100 opacity-90 text-white p-4 transform translate-y-full group-hover:translate-y-0 transition-transform duration-500 h-full">
                         <p className='text-blue-900 font-bold font-merri'>AlterWhite</p>
                     </div>
                 </div>
@@ -30,7 +36,7 @@ const Projects = () => {
                     <div className="w-full h-full">
                         <img className='w-full h-full object-cover' src={krafti} alt="" />
                     </div>
-                    <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t from-sky-700 to-sky-100 opacity-80 text-white p-4 transform translate-y-full group-hover:translate-y-0 transition-transform duration-500 h-full">
+                    <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t from-sky-700 to-sky-100 opacity-90 text-white p-4 transform translate-y-full group-hover:translate-y-0 transition-transform duration-500 h-full">
                         <p className='text-blue-900 font-bold font-merri'>Krafti</p>
                     </div>
                 </div>
